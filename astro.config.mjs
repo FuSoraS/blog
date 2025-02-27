07f3f1f6a2101f9de76262ba23ccea1d7539c2ac
FuSoraS <developer.FuSoraS@gmail.com>
2025-02-27 12:27:21 -0300

    refactor: add search bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config'
+import pagefind from 'astro-pagefind'
 
 export default defineConfig({
   site: 'https://FuSoraS.github.io',
   base: 'blog',
+
+  integrations: [
+    pagefind(),
+  ]
 })
