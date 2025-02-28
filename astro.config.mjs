e4fad27b938e786686c5185e97d04056095bf5ce
FuSoraS <developer.FuSoraS@gmail.com>
2025-02-28 09:42:56 -0300

    refactor: delete pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config'
-import pagefind from 'astro-pagefind'
 
 export default defineConfig({
   site: 'https://FuSoraS.github.io',
   base: 'blog',
 
-  integrations: [
-    pagefind(),
-  ]
 })
